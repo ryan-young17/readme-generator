6f358076ed04f59dfabdc0563b8e00dd7730f264
Ryan Young <ryanbyoung1@gmail.com>
2022-12-19 22:00:31 -0500

    added questions in the array

--- index.js
+++ index.js
@@ -2,7 +2,15 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is your project title?",
+    "What is your project description?",
+    "What are the installation instructions?",
+    "What is the usage information?",
+    "Who contributed to the project?",
+    "What are the test instructions?",
+    "Choose a license for your project"
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
