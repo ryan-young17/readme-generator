861470322c46fb734f71cf91448d82ddceb6972f
Ryan Young <ryanbyoung1@gmail.com>
2022-12-20 18:01:17 -0500

    deleted comments

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,3 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license) {
     return `[![License: ${encodeURIComponent(license)}](https://img.shields.io/badge/License-${encodeURIComponent(license)}-yellow.svg)](https://opensource.org/licenses/${encodeURIComponent(license)})`
@@ -8,8 +6,6 @@ function renderLicenseBadge(license) {
   }
 };
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license) {
     return `[${encodeURIComponent(license)}](https://choosealicense.com/licenses/${encodeURIComponent(license)}/`
@@ -18,8 +14,6 @@ function renderLicenseLink(license) {
   }
 };
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (license) {
     return `This project is licensed under the ${renderLicenseLink(license)} license.`;
@@ -28,7 +22,6 @@ function renderLicenseSection(license) {
   }
 };
 
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(response) {
   return `
 # ${response.title}
