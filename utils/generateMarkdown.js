fce92e8a577f54acee38000cf6aba26012b04b15
Ryan Young <ryanbyoung1@gmail.com>
2022-12-20 17:23:36 -0500

    added license badge, link, and section functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,39 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license) {
+    return `[![License: ${encodeURIComponent(license)}](https://img.shields.io/badge/License-${encodeURIComponent(license)}-yellow.svg)](https://opensource.org/licenses/${encodeURIComponent(license)})`
+  } else {
+    return "";
+  }
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license) {
+    return `[${encodeURIComponent(license)}](https://choosealicense.com/licenses/${encodeURIComponent(license)}/`
+  } else {
+    return "";
+  }
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license) {
+    return `This project is licensed under the ${renderLicenseLink(license)} license.`;
+  } else {
+    return "";
+  }
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(response) {
   return `
 # ${response.title}
+
+${renderLicenseBadge(response.license)}
 
 ## Table of Contents
 - [Description](#Description)
@@ -40,7 +60,7 @@ ${response.contributing}
 ${response.test}
   
 ## License\n
-${response.license}
+${renderLicenseSection(response.license)}
 
 ## Questions\n
 ${response.question}`;
